e796ab3379e850f601194db806dd5914ba8296e9
Felix Ortiz <fortiz@scottsdaleaz.gov>
2023-07-16 17:26:53 -0700

    revert

--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "prefix" {
   type        = string
   description = "(Required) This prefix will be included in the name of most resources."
-  default     = null
 }
 
 variable "project" {
   type        = string
   description = "(Required) Application project name."
-  default     = "null"
 }
 
 variable "environment" {
